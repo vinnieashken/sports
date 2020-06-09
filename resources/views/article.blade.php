ff92788af3af35430518dcef05c389d5a03374aa
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-09 09:59:49 +0300

    Added width to facebook comments

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -43,7 +43,7 @@
                                             <p> </p>
                                             <div class="row">
 
-                                                <div class="mx-auto">
+                                                <div class="mx-auto w-100">
                                                     <div id="fb-root"></div>
                                                     <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v6.0"></script>
                                                     <div class="fb-comments" data-href="{{ url('article/'.$article->id.'/'.Str::slug($article->title,'-')) }}" data-width="100%" data-numposts="30"></div>
