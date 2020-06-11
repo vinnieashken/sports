ee92a08859f290d48de7d266e526d5eb1076d69c
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-11 09:24:01 +0300

    added picturepower ad

--- resources/views/includes/css.blade.php
+++ resources/views/includes/css.blade.php
@@ -39,7 +39,6 @@
     var googletag = googletag || {};
     googletag.cmd = googletag.cmd || [];
 </script>
-
 <script type='text/javascript'>
     googletag.cmd.push(function() {
         var leaderboard1 = googletag.sizeMapping().addSize([1050, 200], [728, 90]).addSize([1024, 768], [728, 90]).addSize([980, 690], [728, 90]).addSize([500, 500], [468, 60]).addSize([0, 0], [320, 50]).build();
@@ -47,7 +46,7 @@
 
         var gameyetutop = googletag.defineSlot('/24409412/gameyetu_top_advert', [[320, 50], [468, 60], [728, 90]], 'div-gpt-ad-1506941653033-0').defineSizeMapping(leaderboard1).addService(googletag.pubads());
         var gameyetuadvert1 = googletag.defineSlot('/24409412/gameyetu_rightpanel_advert1',[ [300, 250],[336, 280]], 'div-gpt-ad-1485837036191-0').defineSizeMapping(squarebanner).addService(googletag.pubads());
-        var gameyetuadvert2 = googletag.defineSlot('/24409412/gameyetu_rightpanel_advert2', [[ [300, 250],[336, 280]], 'div-gpt-ad-1485837098208-0').defineSizeMapping(squarebanner).addService(googletag.pubads());
+        var gameyetuadvert2 = googletag.defineSlot('/24409412/gameyetu_rightpanel_advert2', [ [300, 250],[336, 280]], 'div-gpt-ad-1485837098208-0').defineSizeMapping(squarebanner).addService(googletag.pubads());
         var gameyetuadvert3 = googletag.defineSlot('/24409412/gameyetu_rightpanel_advert3', [ [300, 250],[336, 280]], 'div-gpt-ad-1485837217066-0').defineSizeMapping(squarebanner).addService(googletag.pubads());
         googletag.defineSlot('/24409412/gameyetu_floating_banner', [[320, 50], [468, 60], [728, 90]], 'div-gpt-ad-1579702544837-0').defineSizeMapping(squarebanner).addService(googletag.pubads());
         var gameyetuleaderboard = googletag.defineSlot('/24409412/gameyetu_home_leaderboard2', [728, 90], 'div-gpt-ad-1487330276546-0').defineSizeMapping(leaderboard1).addService(googletag.pubads());
