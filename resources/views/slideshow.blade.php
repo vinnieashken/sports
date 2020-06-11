34dc0bb203525021840c60329886042fac6e79db
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-11 13:09:25 +0300

    Added slideshows functionality

--- resources/views/slideshow.blade.php
+++ resources/views/slideshow.blade.php
@@ -29,76 +29,22 @@
             </div>
             <div class="row justify-content-center">
                 <div class="col-md-12 col-sm-12 aut">
-                    <h2 class="text-succes mb-3 w-75">Has Paul Pogba changed his mind on leaving Man United?</h2>
+                    <h2 class="d-none text-succes mb-3 w-75">Has Paul Pogba changed his mind on leaving Man United?</h2>
                     <!--                <div class="justify-content-around"><span>uytguytguy</span><span>uytguytguy</span><span>uytguytguy</span></div>-->
                     <div class="row">
+
+                        @foreach($pictures as $picture)
                         <div class="col-md-4">
                             <div class="card mb-4 single-standard">
                                 <div class="standard-image">
-                                    <img class="card-img-top" src="https://static01.nyt.com/images/2020/03/08/sports/08onsoccer4/08onsoccer4-mediumSquareAt3X.jpg" alt="Card image cap" onclick="openModal();currentSlide(1)">
-                                </div>
-                            </div>
-                            <div class="card mb-4 single-standard">
-                                <div class="standard-image">
-                                    <img class="card-img-top" src="https://sm.imgix.net/19/06/marcos-rojo.jpg?w=640&h=480&auto=compress,format&fit=clip" alt="Card image cap" onclick="openModal();currentSlide(1)">
-                                </div>
-                            </div>
-                            <div class="card mb-4 single-standard">
-                                <div class="standard-image">
-                                    <img class="card-img-top" src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2019/09/19/21/man-utd-astana-190919zk.jpg?width=1000&height=614&fit=bounds&format=pjpg&auto=webp&quality=70&crop=16:9,offset-y0.5" alt="Card image cap" onclick="openModal();currentSlide(1)">
-                                </div>
-                            </div>
-                            <div class="card mb-4 single-standard">
-                                <div class="standard-image">
-                                    <img class="card-img-top" src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2019/08/11/18/harry-maguire.jpg?w968" alt="Card image cap" onclick="openModal();currentSlide(1)">
+                                    <img class="card-img-top" src="{{ env('IMAGECDN').$picture->imgURL }}" alt="Card image cap" onclick="openModal();currentSlide(1)">
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4">
-                            <div class="card mb-4 single-standard">
-                                <div class="standard-image">
-                                    <img class="card-img-top" src="https://cdn.standardmedia.co.ke/images/monday/manchester_citys_st5ede076e4e184.jpg" alt="Card image cap" onclick="openModal();currentSlide(5)">
-                                </div>
-                            </div>
-                            <div class="card mb-4 single-standard">
-                                <div class="standard-image">
-                                    <img class="card-img-top" src="https://serverlessimageresize-imagebucket-1k649d8ug3han.s3.amazonaws.com/blog/2020/02/man-united-rashford.jpg" alt="Card image cap" onclick="openModal();currentSlide(2)">
-                                </div>
-                            </div>
-                            <div class="card mb-4 single-standard">
-                                <div class="standard-image">
-                                    <img class="card-img-top" src="https://www.completesports.com/wp-content/uploads/2020/02/IMG_20200201_114449.jpg" alt="Card image cap" onclick="openModal();currentSlide(3)">
-                                </div>
-                            </div>
-                            <div class="card mb-4 single-standard">
-                                <div class="standard-image">
-                                    <img class="card-img-top" src="https://manchesterunitedlatestnews.com/wp-content/uploads/2020/01/Manchester-United-Latest-Signings-2020-All-New-Signings-And-Done-Deals-5-Years-scaled.jpg" alt="Card image cap" onclick="openModal();currentSlide(4)">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card mb-4 single-standard">
-                                <div class="standard-image">
-                                    <img class="card-img-top" src="https://cdn.standardmedia.co.ke/images/thursday/bayern_munich_in_ger5ee160455f6bd.jpg" alt="Card image cap" onclick="openModal();currentSlide(5)">
-                                </div>
-                            </div>
-                            <div class="card mb-4 single-standard">
-                                <div class="standard-image">
-                                    <img class="card-img-top" src="https://cdn.standardmedia.co.ke/images/wednesday/brazilian_star_neyma5ee073ffb77e7.jpg" alt="Card image cap" onclick="openModal();currentSlide(2)">
-                                </div>
-                            </div>
-                            <div class="card mb-4 single-standard">
-                                <div class="standard-image">
-                                    <img class="card-img-top" src="https://cdn.standardmedia.co.ke/images/wednesday/everton_players_acce5ee078ba6e48d.jpg" alt="Card image cap" onclick="openModal();currentSlide(3)">
-                                </div>
-                            </div>
-                            <div class="card mb-4 single-standard">
-                                <div class="standard-image">
-                                    <img class="card-img-top" src="https://cdn.standardmedia.co.ke/images/wednesday/pierre_nkurunziza_an5ee0c433ceaf1.jpg" alt="Card image cap" onclick="openModal();currentSlide(4)">
-                                </div>
-                            </div>
-                        </div></div>
-                    <h5 class="more my-4">LOAD MORE</h5>
+                       @endforeach
+
+                    </div>
+                    <h5 class="d-none more my-4">LOAD MORE</h5>
 
                 </div>
             </div>
