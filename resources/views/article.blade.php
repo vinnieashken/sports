50455caee5801d8b7545615ab65e9e023e76e81d
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-12 19:08:37 +0300

    added fill related with latest

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -163,7 +163,7 @@
                             <div class="card-body">
 
                                 <div>
-                                    {!! $articles->renderInAds($article->story,$stories->related,2) !!}
+                                    {!! $articles->renderInAds($article->story,$stories->related,3) !!}
                                 </div>
                             </div>
 
