86096064dd39623eac83914d14434216b0eac887
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-17 21:06:59 +0300

    changed latest query

--- app/Utils/Articles.php
+++ app/Utils/Articles.php
@@ -33,7 +33,8 @@ class Articles
         $parent = Category::on('mysql')->where('site','main')->whereNull('inactive')->where('parentid',0)->where('name','like','%sports%')->first();
         $categories = Category::on('mysql')->whereNull('inactive')->where('parentid',$parent->id)->get(['id'])->pluck('id');
         //->whereNotNull('homepagelistorder')->where('listorder','>',0)
-        return Article::on('mysql')->orderBy('publishday','DESC')->orderBy('homepagelistorder','ASC')->orderBy('listorder','ASC')
+        return Article::on('mysql')
+            ->orderBy('publishdate','DESC')->orderBy('homepagelistorder','ASC')->orderBy('listorder','ASC')
             ->whereNull('inactive')->whereIn('categoryid',$categories)->where('publishday','<=',date('Y-m-d'))
             ->offset($offset)->limit($size)
             ->get(['id','categoryid','title','thumbURL','summary','author','publishday']);
