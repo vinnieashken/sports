d8f837b9333baf2484b141f40908984d7c90b88f
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-19 09:08:03 +0300

    changed fb comments

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -168,6 +168,8 @@
                                             <p> </p>
                                             <div class="row">
 
+
+                                                <p>HELLO</p>
                                                 <div class="mx-auto w-100">
                                                     <div id="fb-root"></div>
                                                     <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v6.0"></script>
