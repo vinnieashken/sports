29f7e235bb09b5e8f65a257d5b027e73941de1d1
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-19 13:44:00 +0300

    updated auth feedbacks

--- resources/views/includes/alert.blade.php
+++ resources/views/includes/alert.blade.php
@@ -10,3 +10,27 @@
         </button>
     </div>
 @endif
+
+@if(\Illuminate\Support\Facades\Session::has('loginerror'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert" style="max-height: 70px !important;">
+        <p class="text-center">
+            <strong >
+                {{ \Illuminate\Support\Facades\Session::get('loginerror') }}
+            </strong></p>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
+
+@if(\Illuminate\Support\Facades\Session::has('registrationerror'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert" style="max-height: 70px !important;">
+        <p class="text-center">
+            <strong >
+                {{ \Illuminate\Support\Facades\Session::get('registrationerror') }}
+            </strong></p>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
