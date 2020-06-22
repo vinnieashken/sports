8271c8e3d20152f41e4589f2409480f444af7f21
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-22 16:30:11 +0300

    fixed checkpoint title length

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -194,7 +194,7 @@
                                     </div>
                                     <div class="media-body ml-3 ml-md-0">
                                         <h6 class="greentext mb-1">{{$articles->getCategory($article->categoryid)->name}}</h6>
-                                        {{ $articles->shorten($articles->getCategory($article->categoryid)->name,$article->title, 48)}}
+                                        {{ $articles->shorten($articles->getCategory($article->categoryid)->name,$article->title, 45)}}
                                     </div>
                                 </div>
                                 </a>
