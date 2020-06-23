1b014316b06895dbf56b36a4f71dfeb39a502c18
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-23 18:00:36 +0300

    added carousel for mobile

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -224,7 +224,7 @@
                                         <div id="carouselExampleControls" class="carousel slide d-md-none d-lg-none d-block" data-ride="carousel">
                                             <div class="carousel-inner">
                                                 @foreach($stories->related as $rarticle)
-                                                <div class="carousel-item active">
+                                                <div class="carousel-item">
                                                     <div class="card bg-dark text-white">
                                                         <a href="{{ url( Str::slug($articles->getCategory($rarticle->categoryid)->name,'-').'/'.$rarticle->id.'/'.str_replace(array('\'', '"'), '', Str::slug( $rarticle->title,'-') ) ) }}">
                                                             <img class="card-img px300" style="height: 300px !important;" src="https://cdn.standardmedia.co.ke{{ $rarticle->thumbURL }}" alt="{{ $rarticle->title }}">
