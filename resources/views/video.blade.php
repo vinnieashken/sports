b04e7c23826730c9c7fe4961688bfa4dacf30aa3
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-24 07:56:18 +0300

    moved more button

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -22,7 +22,7 @@
         <div class="row">
             <div class="offset-md-3 col-md-8 col-sm-12 mb-3">
                 <div style="width:100%;  padding: 0px;" class="mx-auto">
-                    <div id='div-gpt-ad-1506941653033-0' style='width:100%;'>
+                    <div id='div-gpt-ad-1506941653033-0' style='width:100%;' class="mx-auto">
                         <script>
                             googletag.cmd.push(function() { googletag.display('div-gpt-ad-1506941653033-0'); });
                         </script>
