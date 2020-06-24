cb7c691802c2525d7651534b57f4a6c925161719
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-24 11:16:15 +0300

    added author stories

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/category/more/{id}/{offset}','HomeController@categorymore');
 
 Route::get('/article/{id}/{slug}','HomeController@oldarticle');
 Route::get('/article/{id}','HomeController@oldarticleslugless');
+Route::get('/author/{slug}','HomeController@author');
+
 Route::get('/videos','HomeController@videos');
 Route::get('/video/{id}/{slug}','HomeController@video');
 Route::get('/videos/more/{offset}','HomeController@videosmore');
