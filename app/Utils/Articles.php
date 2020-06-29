2934dc24116884f470f2f7ee7ec24d6fd22e53ec
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-29 19:12:04 +0300

    changed io code

--- app/Utils/Articles.php
+++ app/Utils/Articles.php
@@ -273,6 +273,8 @@ class Articles
         $headers = $response->getHeaders();
         $statusCode = $response->getStatusCode();
         $body = $response->getBody()->getContents();
+        dump($body);
+        return;
         $objbody = json_decode($body);
 
         $sports = [];
