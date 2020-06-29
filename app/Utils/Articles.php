ec5d48c747daacc106f1f4105172e26047062ce8
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-29 21:51:07 +0300

    changed twitter feeds js

--- app/Utils/Articles.php
+++ app/Utils/Articles.php
@@ -251,7 +251,7 @@ class Articles
                 'period' => [
                     'name' => 'today'
                 ],
-                'per_page' => 2000
+                'per_page' => $size
             ]
         ];
         $url = 'https://api.onthe.io/ata6CLk8UhmPPvS3PfZYx5wKAloQz24K';
