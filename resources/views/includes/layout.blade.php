9fd7a44e3b9bbc6a279d43033ebeb0107a610447
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-30 10:57:10 +0300

    fixed title

--- resources/views/includes/layout.blade.php
+++ resources/views/includes/layout.blade.php
@@ -3,9 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>
-
-        Sports | @yield('title')
-
+        @yield('title') : The standard Sports
     </title>
     <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no' >
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
