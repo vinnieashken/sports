85f14e4b01245e849c8cdacccc4ae90654591a78
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-06-30 09:03:41 +0300

    fixed related old archives fetching

--- resources/views/includes/layout.blade.php
+++ resources/views/includes/layout.blade.php
@@ -3,11 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>
-        @if(!is_null($article))
-            @yield('title')
-        @else
-            Sports | @yield('title')
-        @endif
+
+        Sports | @yield('title')
 
     </title>
     <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no' >
