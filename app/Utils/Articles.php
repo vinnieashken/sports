f8df0f1d80ffddadc297f2db8c34da61b127b10a
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-07-06 12:11:57 +0300

    added io

--- app/Utils/Articles.php
+++ app/Utils/Articles.php
@@ -300,7 +300,7 @@ class Articles
             ],
             'options' => [
                 'period' => [
-                    'name' => 'Today'
+                    'name' => 'hour'
                 ],
                 'per_page' => 2300,
             ]
