cb5d4c92561fed5c63a740d3195e77b8735332ed
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-07-06 11:55:59 +0300

    added alexa script

--- resources/views/includes/js.blade.php
+++ resources/views/includes/js.blade.php
@@ -18,6 +18,12 @@
 <script src="{{ asset('assets/js/bootstrap.min.js')}}"></script>
 <script src="{{ asset('assets/js/lazy.js')}}"></script>
 
+<script type="text/javascript">
+    _atrk_opts = { atrk_acct:"XVpWq1Y1Mn20Io", domain:"standardmedia.co.ke",dynamic: true};
+    (function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://certify-js.alexametrics.com/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+</script>
+<noscript><img src="https://certify.alexametrics.com/atrk.gif?account=XVpWq1Y1Mn20Io" style="display:none" height="1" width="1" alt="" /></noscript>
+
 <script>
     function openNav() {
         document.getElementById("mySidenav").style.width = "250px";
