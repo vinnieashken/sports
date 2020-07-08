7b28da28de49742bb0066deed319a6f99877e6f4
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-07-08 12:34:13 +0300

    changed amp url

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -134,6 +134,9 @@ class HomeController extends Controller
 
     public function article($category_slug,$id,$slug)
     {
+        if(!is_numeric($id))
+            return redirect('/');
+
         $menu = new Menu();
         $timeutil = new TimeUtil();
         $categories = $menu->getCategories();
