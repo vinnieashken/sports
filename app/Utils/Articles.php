49e5f3899b34b6bbd3c6d8af3e44a9930297e072
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-07-10 16:34:37 +0300

    added amp

--- app/Utils/Articles.php
+++ app/Utils/Articles.php
@@ -305,7 +305,8 @@ class Articles
 
     public function renderAmp($story)
     {
-        $data = preg_replace('/(<img[^>]+>(?:<\/img>)?)/i', '$1</amp-img>', $story);
+        $data = preg_replace('/<img[^>]+\/>/i', '$1</amp-img>', $story);
+        $data = str_replace('<img', '<amp-img', $data);
         return str_replace('/images',env('IMAGECDN').'/images',$data);
     }
 
