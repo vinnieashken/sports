2630de584847d5d0af28233def7b97acfa7d592a
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-07-10 19:24:08 +0300

    added amp

--- resources/views/amp/article.blade.php
+++ resources/views/amp/article.blade.php
@@ -12,17 +12,17 @@
         </header>
 
         <div class="socialmedia-buttons btn-group mb-4">
-            <a href="https://www.facebook.com/sharer.php?u=https://www.standardmedia.co.ke/the-standard-insider/article/2001378263/retirement-blues-unlike-footballers-rugby-players-dont-omba-serikali" class="btn btn-facebook rounded-0 text-white">
+            <a href="https://www.facebook.com/sharer.php?u={{ url()->current() }}" class="btn btn-facebook rounded-0 text-white">
                 <span class="fa fa-facebook"></span>
             </a>
-            <a href="https://web.whatsapp.com/send?text=https://www.standardmedia.co.ke/the-standard-insider/article/2001378263/retirement-blues-unlike-footballers-rugby-players-dont-omba-serikali" class="btn btn-whatsapp rounded-0 text-white">
-                <span class="fa fa-whatsapp"></span>
-            </a>
-            <a href="https://twitter.com/share?url=https://www.standardmedia.co.ke/the-standard-insider/article/2001378263/retirement-blues-unlike-footballers-rugby-players-dont-omba-serikali&amp;hashtags=StandardDigital&amp;text=Retirement+blues%3A+Unlike+footballers%2C+rugby+players+don%E2%80%99t+%E2%80%98omba+serikali%E2%80%99" class="btn btn-twitter rounded-0 text-white">
+            <a href="https://twitter.com/share?url={{ url()->current() }}&hashtags={{ ' ' }}&text={{ urlencode($article->title) }}" class="btn btn-twitter rounded-0 text-white">
                 <span class="fa fa-twitter texu-white"></span>
             </a>
-            <a href="https://telegram.me/share/url?url=https://www.standardmedia.co.ke/the-standard-insider/article/2001378263/retirement-blues-unlike-footballers-rugby-players-dont-omba-serikali&amp;text=Retirement blues: Unlike footballers, rugby players don’t ‘omba serikali’" class="btn btn-telegram rounded-0 text-white">
+            <a href="https://telegram.me/share/url?url={{ url()->current() }}&text={{  $article->title }}" class="btn btn-telegram rounded-0 text-white">
                 <span class="fa fa-telegram"></span>
+            </a>
+            <a href="whatsapp://send?text={{ $article->title.' – '.( url()->current()) }}&utm-source=whatsapp&utm-medium=share&utm-campaign={{ $article->title }}" class="btn btn-whatsapp rounded-0 text-white d-block d-md-none" >
+                <span class="fa fa-whatsapp"></span>
             </a>
         </div>
 
