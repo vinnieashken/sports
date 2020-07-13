93aac0df3a8a7b274d6a499ffa4613174d5c9c3a
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-07-13 14:37:37 +0300

    added amp page

--- resources/views/amp/article.blade.php
+++ resources/views/amp/article.blade.php
@@ -59,6 +59,33 @@
 
         {!! $articles->renderAmp($article->story,$stories->related,2) !!}
 
+        <div class=" ">
+            <h4 class="section-heading my-4">FOLLOW US ON</h4>
+            <div class="d-flex mx-auto my-2 follow">
+                <div>
+                    <div class="socialmedia-buttons btn-group text-center mx-auto">
+
+                        <a href="https://www.facebook.com/GameYetu/"
+                           class="btn btn-facebook rounded-0 text-white" style="
+                                background: #3b5998;"><span class="fa fa-facebook texu-white"></span></a>
+
+                        <a href="https://twitter.com/gameyetu?lang=en"
+                           class="btn btn-facebook rounded-0 text-white" style="
+                                background: rgb(0, 126, 196);"><span class="fa fa-twitter texu-white"></span></a>
+
+                        <a href="https://t.me/gameyetu"
+                           class="btn btn-facebook rounded-0 text-white" style="
+                                background: #0088cc;"><span class="fa fa-telegram texu-white"></span></a>
+
+                        <a href="https://www.instagram.com/game_yetu/?hl=en"
+                           class="btn btn-facebook rounded-0 text-white" style="
+                                background: #d72978;"><span class="fa fa-instagram texu-white"></span></a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <amp-ad width=300 height=250 layout="responsive" type="doubleclick" data-slot="/24409412/gameyetu_rightpanel_advert3" data-multi-size="336x280">
         </amp-ad>
     </article>
