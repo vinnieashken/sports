5648a0fce295035d33d259f148afc2b0b434322c
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-07-14 12:18:28 +0300

    added amp page

--- resources/views/includes/js.blade.php
+++ resources/views/includes/js.blade.php
@@ -15,7 +15,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
         crossorigin="anonymous" defer></script>
-<script src="{{ asset('assets/js/bootstrap.min.js')}}" defer></script>
+<script src="{{ asset('assets/js/bootstrap.min.js')}}" async></script>
 <script src="{{ asset('assets/js/lazy.js')}}" defer></script>
 
 <script>
