c60ba683db0ed61c6084fe11581e683a733e0e0c
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-07-16 09:59:35 +0300

    added cookie consent

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,13 +1,20 @@
+<style>
+    .cookie-btn {
+        color: #fff!important;
+        background: #38ab44;
+    }
+</style>
+
 <div class="container fixed-bottom">
     <div class="alert bg-black js-cookie-consent cookie-consent">
 
         <div class="text-white cookie-consent__message">
             {!! trans('cookieConsent::texts.message') !!}
         </div>
-
-        <button class="btn green js-cookie-consent-agree cookie-consent__agree">
+     <div class="d-flex ">
+        <button class="btn cookie-btn js-cookie-consent-agree cookie-consent__agree ml-auto">
             {{ trans('cookieConsent::texts.agree') }}
         </button>
-
+     </div>
     </div>
 </div>
