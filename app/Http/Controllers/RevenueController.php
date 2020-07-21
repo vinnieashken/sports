af60174f75bb33b46e6deb8e8853e54bcb1dc2b6
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-07-21 12:34:26 +0300

    added account check functionality

--- app/Http/Controllers/RevenueController.php
+++ app/Http/Controllers/RevenueController.php
@@ -246,6 +246,7 @@ class RevenueController extends Controller
         if((int)$response->getStatusCode() >= 400 )
         {
             $request->session()->flash('registrationerror', 'Your email is not registered.Fill in the form to create an account');
+            $request->session()->flash('email', $email);
             return redirect(URL::previous());
         }
 
