79f5152e33d63467a09f58afc605726a06e3c16d
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-07-27 13:44:31 +0300

    added social icons classes

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -141,10 +141,10 @@
                                         <a href="whatsapp://send?text={{ $article->title.' – '.( url()->current()) }}&utm-source=whatsapp&utm-medium=share&utm-campaign={{ $article->title }}" class="btn btn-whatsapp btn_whatsapp rounded-0 text-white d-block d-md-none" >
                                             <span class="fa fa-whatsapp"></span>
                                         </a>
-                                        <a href="" class="btn btn-linkedin btn_linkedin rounded-0 text-white">
+                                        <a href="https://www.linkedin.com/shareArticle?url={{ url()->current() }}&title={{  $article->title }}&summary={{  $article->summary ?? "" }}&source=The Standard" class="btn btn-linkedin btn_linkedin rounded-0 text-white">
                                             <span  class="fa fa-linkedin"></span>
                                         </a>
-                                        <a href="" class="btn btn-email btn_email rounded-0 text-white">
+                                        <a href="mailto:?subject={{ $article->title }}&amp;body={{ $article->title }}<br>{{  $article->summary ?? "" }} link : {{ url()->current() }} ." class="btn btn-email btn_email rounded-0 text-white">
                                             <span  class="fa fa-envelope"></span>
                                         </a>
 
