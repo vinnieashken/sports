a9aed48a27ba7b7de10c361dcc2072af2d03ba53
Vincent Mutune <VMutune@STANDARDMEDIA.CO.KE>
2020-08-06 16:08:20 +0300

    added changes amp url

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -46,6 +46,35 @@ class MobileController extends Controller
         return view('amp.article',['timeutil'=> $timeutil,'article'=>$article,'articles'=> $articles,'categories'=>$categories,'stories' => $stories]);
     }
 
+    public function articleold($id,$slug)
+    {
+        if(!is_numeric($id))
+            return redirect('/');
+
+        $menu = new Menu();
+        $timeutil = new TimeUtil();
+        $categories = $menu->getCategories();
+        $articles = new Articles();
+        $article = $articles->getArticle($id);
+        $stories = new \stdClass();
+        $stories->url = str_replace('amp/','',url()->current());
+        $stories->related = $articles->getRelatedArticles($id,6,0);
+
+        if($stories->related->count() < 6)
+        {
+            $latest = $articles->getLatestExcept($id, (6 - $stories->related->count()),0);
+
+            foreach ($latest as $item)
+            {
+                $stories->related->push($item);
+            }
+
+        }
+        $stories->latest = $articles->getLatest(5,0);
+
+        return view('amp.article',['timeutil'=> $timeutil,'article'=>$article,'articles'=> $articles,'categories'=>$categories,'stories' => $stories]);
+    }
+
     public function getCookie(Request $request)
     {
         $cookie = Cookie::get('entertainment_story_no');
